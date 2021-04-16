c65a3628a39119a3f98713bc65b3e092ab411341
manchikantisaikiran <manchikantisaikiran777@gmail.com>
2021-04-16 19:47:09 +0530

    reactive forms example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { passwordChecker } from './custom-validators/password-checker'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'signup-reactive';
+
+
+  signUpForm!: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) { }
+
+  ngOnInit() {
+    this.signUpForm = this.formBuilder.group({
+      firstName: ['', [Validators.required, Validators.minLength(2)]],
+      lastName: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required,Validators.minLength(6)]],
+      confirmPassword: ['', Validators.required],
+      isAccepted: [false, Validators.requiredTrue]
+    }, {
+      validators: passwordChecker('password', 'confirmPassword')
+    })
+  }
+
+  get Helper() {
+    return this.signUpForm.controls
+  }
+
+  onSubmit() {
+    this.submitted = true;
+    console.log(this.signUpForm)
+    console.log(this.Helper);
+    
+    if (this.signUpForm.invalid) {
+      return;
+    }
+
+    console.table(this.signUpForm.value)
+    console.table(this.signUpForm)
+
+    alert('success')
+  }
+
+  onReset() {
+    this.submitted = false;
+    this.signUpForm.reset();
+  }
 }
